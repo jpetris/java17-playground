3aa5639185ca7a90499c81e369f10ba42d655666
Jorge Petris <jorgepetris92@gmail.com>
2026-03-25 10:20:18 -0600

    initialize Spring Boot application with basic configuration and test setup

---
+++ src/main/java/com/jorgepetris/java17playground/Java17PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jorgepetris.java17playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17PlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgepetris/java17playground/Java17PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgepetris.java17playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
