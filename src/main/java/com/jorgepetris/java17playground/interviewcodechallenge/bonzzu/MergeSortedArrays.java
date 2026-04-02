38f4d5d39d8a2730f4b04ba73ef2f56c260bd38e
Jorge Petris <jorgepetris92@gmail.com>
2026-04-02 00:25:14 -0600

    Add MergeSortedArrays class and update .gitignore for IDE files

---
+++ src/main/java/com/jorgepetris/java17playground/interviewcodechallenge/bonzzu/MergeSortedArrays.java
@@ -0,0 +1,21 @@
+package com.jorgepetris.java17playground.interviewcodechallenge.bonzzu;
+
+/**
+ * You are given an array of sorted arrays (integers)
+ * Your task is to merge them into a single array
+ *
+ * Restrictions:
+ * - You can't use built-in function to sort the whole array
+ * - You can't use complex data structures such as treeset, hashset, etc
+ *
+ * Input: args int[int[]]
+ * Output: int[]
+ *
+ * Examples:
+ * 1. Input: {1,2,3,4}, {10,15,21} | Output: {1,2,3,4,10,15,21}
+ *
+ */
+
+public class MergeSortedArrays {
+
+}
